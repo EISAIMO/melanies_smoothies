4ebe725dafab938b13d1b4dc56100d1fe19a1095
EISAIMO <isaias.avelino.moreno@ericsson.com>
2026-06-10 13:25:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,5 @@ if ingredients_list:
 # new session to display smoothiefroot nutrition information
 import requests  
 smoothiefroot_response = requests.get('https://my.smoothiefroot.com/api/fruit/watermelon')  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
